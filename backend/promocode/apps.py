7754830f6867cafa767867f5c491bb3d8ff9428f
CuperEvgeha1400 <novapiple228@gmail.com>
2023-08-21 17:48:26 +0400

    added automatic addition of stripe promo codes

--- backend/promocode/apps.py
+++ backend/promocode/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class PromocodeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'promocode'
+    def ready(self):
+        import promocode.signals
