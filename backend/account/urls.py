91d6b4638cb754510ae2ea678e851ba8fedae1e6
CuperEvgeha1400 <novapiple228@gmail.com>
2023-10-25 18:32:27 +0400

    fix authemail templates and verify

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MyUserView, TokenView
+from .views import MyUserView, TokenView, SignupVerify
 
 urlpatterns = [
     path('MyUser/', MyUserView.as_view()),
-    path('TokenView/', TokenView)
+    path('TokenView/', TokenView),
+    path('SignupVerify/', SignupVerify.as_view())
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -2,9 +2,12 @@ from requests import request
 from rest_framework import generics, status
 from rest_framework.decorators import permission_classes, api_view
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from authemail.models import SignupCode, EmailChangeCode, PasswordResetCode
 from authemail import wrapper
+
 from .models import User
 from .serializer import MyUserSerializer
 
@@ -24,3 +27,23 @@ def TokenView(request):
     account = wrapper.Authemail()
     response = account.login(email=email, password=password)
     return Response(response)
+
+
+class SignupVerify(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request, format=None):
+        code = request.GET.get('code', '')
+        verified = SignupCode.objects.set_user_is_verified(code)
+
+        if verified:
+            try:
+                signup_code = SignupCode.objects.get(code=code)
+                signup_code.delete()
+            except SignupCode.DoesNotExist:
+                pass
+            content = {'success': ('Email address verified.')}
+            return Response(content, status=status.HTTP_200_OK)
+        else:
+            content = {'detail': ('Unable to verify user.')}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
