2e2e7b7c1663922563dd4469710d1272bdae80f6
CuperEvgeha1400 <novapiple228@gmail.com>
2023-11-27 20:08:35 +0400

    fix bugs

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 from .models import Review
 
 
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
+
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
-        depth = 2
         fields = ('id', 'content', 'rating', 'product', 'author')
