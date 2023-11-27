66fe04482bef88638a65d3f49464ff822624078b
CuperEvgeha1400 <novapiple228@gmail.com>
2023-11-27 18:38:23 +0400

    fix rating

--- backend/reviews/models.py
+++ backend/reviews/models.py
@@ -5,7 +5,7 @@ from account.models import User
 class Review(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    rating = models.PositiveIntegerField()
+    rating = models.PositiveIntegerField(blank=True, null=True)
     product = models.ForeignKey(BaseProduct, on_delete=models.CASCADE, related_name='reviews')
 
     def __str__(self):
