9494b338516aeec6474496c820277860ee399bc7
CuperEvgeha1400 <novapiple228@gmail.com>
2023-12-03 16:52:49 +0100

    test

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-until python manage.py migrate --noinput
+
+until python manage.py makemigrations && python manage.py migrate --noinput 
 do
     echo "Waiting for db to be ready..."
     sleep 2
